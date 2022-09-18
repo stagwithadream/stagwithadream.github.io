897a47a528dde7631d7de83d6cfcea57ec618062
Sairam Reddy <35459972+stagwithadream@users.noreply.github.com>
2022-09-17 20:26:26 -0700

    main

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stagwithadream/stagwithadream.github.io.git/precache-manifest.9183a7b2992b3e39e21bcaa4e7726be7.js"
+  "/precache-manifest.4096b751d2db0622bf2707d6a654ebb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stagwithadream/stagwithadream.github.io.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
